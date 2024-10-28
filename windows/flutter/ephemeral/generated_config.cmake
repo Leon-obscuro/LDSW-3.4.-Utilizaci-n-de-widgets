26348db7e45d207461bc21742104ecd5721d1edb
Leon-obscuro <112833230+Leon-obscuro@users.noreply.github.com>
2024-10-27 22:19:40 -0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter_windows_3.24.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\LDSW_utilizacion de widgets\\flutter_widgets" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter_windows_3.24.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\LDSW_utilizacion de widgets\\flutter_widgets"
+  "FLUTTER_ROOT=C:\\dev\\flutter_windows_3.24.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\LDSW_utilizacion de widgets\\flutter_widgets\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\LDSW_utilizacion de widgets\\flutter_widgets"
+  "FLUTTER_TARGET=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\LDSW_utilizacion de widgets\\flutter_widgets\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\LDSW_utilizacion de widgets\\flutter_widgets\\.dart_tool\\package_config.json"
+)
